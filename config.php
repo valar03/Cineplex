e7333480b0d4c893610463a0617f9790e5f614d3
Valarmathi B <106001202+valar03@users.noreply.github.com>
2023-07-11 13:17:59 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $user = "root";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
-    $db = "db_movie";                                  //Your database name you want to connect to
+    $db = "cineplex";                                  //Your database name you want to connect to
     //$port = 3306;
      $con = mysqli_connect($host, $user, $pass, $db)or die(mysql_error());
 ?>
