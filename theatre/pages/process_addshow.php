c48eb4fa073f5b4272bd367bd6d914b0922ed3f2
Valarmathi B <106001202+valar03@users.noreply.github.com>
2023-07-17 22:38:37 +0530

    Update process_addshow.php

--- theatre/pages/process_addshow.php
+++ theatre/pages/process_addshow.php
@@ -4,7 +4,7 @@
     extract($_POST);
     foreach($stime as $time)
     {
-        mysqli_query($con,"INSERT INTO `tbl_shows`(`s_id`, `st_id`, `theatre_id`, `movie_id`, `start_date`, `status`, `r_status`, `nos`) values(NULL,'$time','".$_SESSION['theatre']."','$movie','$sdate','1','0','0')");
+        mysqli_query($con,"INSERT INTO `tbl_shows`(`s_id`, `st_id`, `theatre_id`, `movie_id`, `start_date`, `status`, `r_status`) values(NULL,'$time','".$_SESSION['theatre']."','$movie','$sdate','1','0')");
 
     }
     $_SESSION['success']="Show Added";
